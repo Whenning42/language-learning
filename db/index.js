801e0c01f74113f8a8070dd233f7097e5d0736c1
William Henning <wchenningwchenning@gmail.com>
2025-02-10 10:55:10 -0500

    Switch to sqlite!

--- db/index.js
+++ db/index.js
@@ -1,33 +1,9 @@
-const config = require("../config/database.js");
-const Sequelize = require("sequelize");
+const  Sequelize = require('sequelize');
+const { SqliteDialect } = require('@sequelize/sqlite3');
 
-const sequelize = new Sequelize(
-  config.development.database,
-  config.development.username,
-  config.development.password,
-  {
-    host: config.development.host,
-    dialect: "mariadb",
-    port: config.development.port,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  }
-);
-
-// Test connection
-async function testConnection() {
-  try {
-    await sequelize.authenticate();
-    console.log("Database connection successful.");
-  } catch (error) {
-    console.error("Unable to connect to db:", error);
-  }
-}
-
-testConnection();
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'project.db',
+});
 
 module.exports = sequelize;
