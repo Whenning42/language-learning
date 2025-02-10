d1e1e5f5a31d418c8f5156f5bda5c99377e69ffd
William Henning <wchenningwchenning@gmail.com>
2025-02-10 11:35:12 -0500

    Use functional style for AddTutorial component

--- client/src/components/add-tutorial.component.js
+++ client/src/components/add-tutorial.component.js
@@ -1,83 +1,66 @@
-import React, {Component} from "react";
-import http from "../http-common";
+import React, {useState}  from "react";
 import styles from "../styles.module.css"
 
-export default class AddTutorial extends Component {
-  constructor(props) {
-    super(props);
-    this.onChangeTitle = this.onChangeTitle.bind(this);
-    this.onChangeDescription = this.onChangeDescription.bind(this);
-    this.onAddTutorialClick = this.onAddTutorialClick.bind(this);
+function AddTutorial (props) {
+  const [title, setTitle] = useState([]);
+  const [description, setDescription] = useState([]);
 
-    this.state = {
-      id: null,
-      title: "",
-      description: "",
-      published: false,
-      submitted: false
-    };
+  const onChangeTitle = async (e) => {
+    setTitle(e.target.value);
   }
 
-  onChangeTitle(e) {
-    this.setState({
-      title: e.target.value
-    });
+  const onChangeDescription = async (e) => {
+    setDescription(e.target.value);
   }
 
-  onChangeDescription(e) {
-    this.setState({
-      description: e.target.value
-    });
+  const onAddTutorialClick = async () => {
+    props.onAddTutorial(title, description, false);
   }
 
-  onAddTutorialClick() {
-    this.props.onAddTutorial(this.state.title, this.state.description, this.state.published)
-  }
-
-  render() {
-    return (
-      <div>
-        <div className={styles.container}>
-          <h2>Add tutorial!</h2>
-          <div className={styles.tutorial_inputs}>
-            <div className={styles.column}>
-              <label htmlFor="title">Title</label>
-              <input
-                type="text"
-                id="title"
-                required
-                value={this.state.title}
-                onChange={this.onChangeTitle}
-                name="title"
-              />
-            </div>
-            <div className={styles.column}>
-              <label htmlFor="description">Description</label>
-              <input
-                type="text"
-                id="description"
-                required
-                value={this.state.description}
-                onChange={this.onChangeDescription}
-                name="description"
-              />
-            </div>
-            <div className={styles.column}>
-              <label htmlFor="published">Published</label>
-              <input
-                type="text"
-                id="published"
-                name="published"
-              />
-            </div>
+  return (
+    <div>
+      <div className={styles.container}>
+        <h2>Add tutorial!</h2>
+        <div className={styles.tutorial_inputs}>
+          <div className={styles.column}>
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              id="title"
+              required
+              value={title}
+              onChange={onChangeTitle}
+              name="title"
+            />
           </div>
-          <div className={styles.submit_div}>
-            <button onClick={this.onAddTutorialClick} className={styles.button}>
-              Submit
-            </button>
+          <div className={styles.column}>
+            <label htmlFor="description">Description</label>
+            <input
+              type="text"
+              id="description"
+              required
+              value={description}
+              onChange={onChangeDescription}
+              name="description"
+            />
+          </div>
+          <div className={styles.column}>
+            <label htmlFor="published">Published</label>
+            <input
+              type="text"
+              id="published"
+              name="published"
+            />
           </div>
         </div>
+        <div className={styles.submit_div}>
+          <button onClick={onAddTutorialClick} className={styles.button}>
+            Submit
+          </button>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default AddTutorial;
