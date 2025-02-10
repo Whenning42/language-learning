6edd37bbae271c35cb169cea38454c1b2124c413
William Henning <wchenningwchenning@gmail.com>
2025-02-10 11:26:57 -0500

    Use functional style in tutorial-list component

--- client/src/components/tutorial-list.component.js
+++ client/src/components/tutorial-list.component.js
@@ -1,40 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 import styles from "../styles.module.css"
 
-export default class TutorialsList extends Component {
-  constructor(props) {
-    super(props);
-  }
+function TutorialsList (props) {
+  const table_rows = props.tutorials.map(tutorial => {
+    return <tr key={tutorial["id"]}>
+      <td>{tutorial["id"]}</td>
+      <td>{tutorial["title"]}</td>
+      <td>{tutorial["description"]}</td>
+      <td>{tutorial["published"]}</td>
+      <td><button onClick={() => props.deleteRow(tutorial["id"])}>Delete</button></td>
+    </tr>
+  });
 
-  render () {
-    const table_rows = this.props.tutorials.map(tutorial => {
-      return <tr key={tutorial["id"]}>
-        <td>{tutorial["id"]}</td>
-        <td>{tutorial["title"]}</td>
-        <td>{tutorial["description"]}</td>
-        <td>{tutorial["published"]}</td>
-        <td><button onClick={() => this.props.deleteRow(tutorial["id"])}>Delete</button></td>
-      </tr>
-    });
-
-    return (
-      <div>
-        <h2>Tutorial List</h2>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Description</th>
-              <th>Published</th>
-              <th>Delete?</th>
-            </tr>
-          </thead>
-          <tbody>
-            {table_rows}
-          </tbody>
-        </table>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h2>Tutorial List</h2>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Published</th>
+            <th>Delete?</th>
+          </tr>
+        </thead>
+        <tbody>
+          {table_rows}
+        </tbody>
+      </table>
+    </div>
+  )
 };
+
+export default TutorialsList;
