c29b38a3296e9d7054688b26c3c93392f8eff0da
William Henning <wchenningwchenning@gmail.com>
2025-02-15 12:47:03 -0500

    Set backend to force no-cache

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
+require('dotenv').config();
 const express = require("express");
 const initializeDatabase = require("./db/init")
 const cors = require("cors");
 const tutorialRoutes = require("./routes/tutorials");
+
+const NO_CACHE = process.env.NO_CACHE === "true";
+if (NO_CACHE) {
+  console.log("Forcing no cache on static cache.");
+}
 
 const app = express();
 app.use(cors());
@@ -11,7 +17,14 @@ app.use(express.urlencoded({extended: true}));
 initializeDatabase();
 
 app.use("/api", tutorialRoutes);
-app.use("/videos", express.static("videos"));
+
+const setNoStoreHeader = (req, res, next) => {
+    if (NO_CACHE) {
+        res.set('Cache-Control', 'no-store');
+    }
+    next();
+};
+app.use("/videos", setNoStoreHeader, express.static("videos"));
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
