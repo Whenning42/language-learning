7c84141539eeea7c576a695da23a2190326a2c44
William Henning <wchenningwchenning@gmail.com>
2025-02-19 18:16:00 -0500

    Update http common library to support configurable backend url

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${backendUrl}/api`,
   headers: {
     "Content-type": "application/json"
   }
