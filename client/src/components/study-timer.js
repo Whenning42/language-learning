c2f44df6e3669113145dec9c1998c4f7dd797bb2
William Henning <wchenningwchenning@gmail.com>
2025-02-20 18:31:35 -0500

    Finish intial study timer implementation and add pause styling

--- client/src/components/study-timer.js
+++ client/src/components/study-timer.js
@@ -3,7 +3,10 @@ import "./study-timer.css"
 
 function StudyTimer() {
     // TODO: Make time tick down.
-    const [timeLeft, setTimeLeft] = useState(0);
+    const [clock, setClock] = useState({
+        timeLeft: 0,
+        isPaused: false
+    });
 
     const renderTime = (seconds) => {
         var minutes = Math.floor(seconds / 60);
@@ -14,31 +17,41 @@ function StudyTimer() {
     }
 
     useEffect(() => {
-        if (timeLeft <= 0) return; // Stop when countdown reaches zero
-
         const timer = setInterval(() => {
-            setTimeLeft((prevTime) => prevTime - 1);
+            setClock((clk) => {
+                if (clk.isPaused) {
+                    return clk;
+                }
+
+                const newTime = Math.max(clk.timeLeft - 1, 0);
+                return {...clk, timeLeft: newTime};
+            });
         }, 1000);
 
         return () => clearInterval(timer); // Cleanup on unmount
-    }, [timeLeft]);
+    }, []);
 
     const changeTimeDelta = 600;
     const onAddTime = () => {
-        setTimeLeft(timeLeft + changeTimeDelta);
+        setClock({...clock, timeLeft: clock.timeLeft + changeTimeDelta});
     }
 
     const onRemoveTime = () => {
-        setTimeLeft(Math.max(timeLeft - changeTimeDelta, 0));
+        setClock({...clock, timeLeft: Math.max(clock.timeLeft - changeTimeDelta, 0)});
     }
 
-    var timeStr = renderTime(timeLeft);
+    const onToggle = () => {
+        setClock({...clock, isPaused: !clock.isPaused});
+    }
+
+    const clock_class = "clock " + (clock.isPaused ? "paused-clock" : "");
     return (
         <div className="timer-div">
             Study Timer
-            <div className="clock">{timeStr}</div>
+            <div className={clock_class}>{renderTime(clock.timeLeft)}</div>
             <button className="timer-button" onClick={onAddTime}>+10 min</button>
             <button className="timer-button" onClick={onRemoveTime}>-10 min</button>
+            <button className="timer-button" onClick={onToggle}>Start/Stop</button>
         </div>
     );
 }
