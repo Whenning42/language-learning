2d3933b5025bc9b677d5a993a65cd64e0885f46c
William Henning <wchenningwchenning@gmail.com>
2025-03-12 13:30:34 -0400

    Add some menu bar styling

--- client/src/components/menu-bar/menu-bar.js
+++ client/src/components/menu-bar/menu-bar.js
@@ -1,14 +1,16 @@
 import React from "react"
 
+import "./menu-bar.css"
+
 function MenuBar() {
   return (
     <div>
-      <div>
-        Watch N' Learn| 
-        <a href="/home">Home</a>| 
-        <a href="/media">Media</a>| 
-        <a href="/note-cards">Notecards</a>| 
-        <a href="/placement-quiz">Placement Quiz</a>| 
+      <div style={{display: "inline-block"}}>
+        <div className="menu-bar">Watch N' Learn</div>
+        <div className="menu-bar"><a href="/home">Home</a></div>
+        <div className="menu-bar"><a href="/media">Media</a></div>
+        <div className="menu-bar"><a href="/note-cards">Notecards</a></div>
+        <div className="menu-bar"><a href="/placement-quiz">Placement Quiz</a></div>
       </div>
     </div>
   )
