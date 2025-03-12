9eceb3ee9ec667b1bf9cdeaa898e9dd04e716b57
William Henning <wchenningwchenning@gmail.com>
2025-03-12 12:35:18 -0400

    Check in enter to continue placement quiz set up

--- client/src/components/placement-quiz/placement-quiz.js
+++ client/src/components/placement-quiz/placement-quiz.js
@@ -1,9 +1,28 @@
-import React from "react"
+import React, {useEffect, useRef} from "react"
 
 function PlacementQuiz () {
+  const welcome_blurb = `
+We'll ask you a series of questions about the language you're learning. Answer as
+best as you can, and we'll use your answers to figure out how much of your target
+language that you know. (Enter to continue)`
+
+  const blurb_div_ref = useRef(null);
+
+  useEffect(() => {
+    if (blurb_div_ref.current) {
+      blurb_div_ref.current.focus();
+    }
+  }, []);
+
+  const onKeyDown = (e) => {
+    if (e.key === "Enter") {
+      console.log("Enter");
+    }
+  }
+
   return (
-    <div>
-      TODO PlacmentQuiz
+    <div onKeyDown={onKeyDown} tabIndex={0} ref={blurb_div_ref}>
+      {welcome_blurb}
     </div>
   )
 }
