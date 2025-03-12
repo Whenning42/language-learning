ff661b7a1bf8957d10576f19b055a7c12335724d
William Henning <wchenningwchenning@gmail.com>
2025-03-12 13:30:52 -0400

    Add some styling to the placement quiz UI

--- client/src/components/placement-quiz/placement-quiz.js
+++ client/src/components/placement-quiz/placement-quiz.js
@@ -1,28 +1,25 @@
 import React, {useEffect, useRef} from "react"
 
+import "./placement-quiz.css"
+
 function PlacementQuiz () {
   const welcome_blurb = `
-We'll ask you a series of questions about the language you're learning. Answer as
-best as you can, and we'll use your answers to figure out how much of your target
-language that you know. (Enter to continue)`
-
-  const blurb_div_ref = useRef(null);
-
-  useEffect(() => {
-    if (blurb_div_ref.current) {
-      blurb_div_ref.current.focus();
-    }
-  }, []);
-
-  const onKeyDown = (e) => {
-    if (e.key === "Enter") {
-      console.log("Enter");
-    }
-  }
+Welcome to the placement quiz. We'll ask you a series of questions about the language
+you're learning. Answer as best as you can, and we'll use your answers to figure out how
+much of your target language you know.`
 
   return (
-    <div onKeyDown={onKeyDown} tabIndex={0} ref={blurb_div_ref}>
-      {welcome_blurb}
+    <div style={{margin: '10px'}}>
+      <div className="quiz-div">{welcome_blurb}</div>
+      <div className="quiz-div">
+        Question 0/0
+      </div>
+      <div className="quiz-div">
+      Do you know the word "Omlette"?
+      </div>
+      <button className="quiz-button">Yes</button>
+      <button className="quiz-button">Maybe / Sort of</button>
+      <button className="quiz-button">No</button>
     </div>
   )
 }
