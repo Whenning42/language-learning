18f506c072e28764e1bc1826bb6b93e567c54b21
William Henning <wchenningwchenning@gmail.com>
2025-03-19 16:10:28 -0400

    Check in latest placement quiz version

--- client/src/components/placement-quiz/placement-quiz.js
+++ client/src/components/placement-quiz/placement-quiz.js
@@ -1,7 +1,23 @@
-import React, {useEffect, useState} from "react"
+import React, {useEffect, useState, useRef} from "react"
 
 import "./placement-quiz.css"
-import http from "../../http-common"
+import {authedHttp} from "../../lib/auth-service"
+
+function randint(max) {
+  return Math.floor(Math.random() * max);
+}
+
+async function get_question(question_num, question_offset) {
+    const question_id = question_num + question_offset;
+    var new_word = ""
+    try {
+      var res = await authedHttp().get(`/placement-quiz/sample-word/${question_id}`)
+      new_word = res.word;
+    } catch (e) {
+      new_word = "mock_q_" + question_num;
+    }
+    return {word: new_word, question_num: question_num};
+}
 
 function PlacementQuiz () {
   const welcome_blurb = `
@@ -9,49 +25,33 @@ Welcome to the placement quiz. We'll ask you a series of questions about the lan
 you're learning. Answer as best as you can, and we'll use your answers to figure out how
 much of your target language you know.`;
 
-  const [randomOffset, setRandomOffset] = useState(Math.floor(Math.random() * 10_000));
-  const [curQuestion, setCurQuestion] = useState(0);
-  const [word, setWord] = useState("");
-
   const num_questions = 100;
-  const user = "demo-user";
+  const questions_offset = useRef(0);
+  const [curQuestion, setCurQuestion] = useState({
+    word: "",
+    question_num: 1,
+  });
 
   useEffect(() => {
-    getNextQuestion();
-  }, []);
-
-  const getNextQuestion = (random_offset=null) => {
-    var offset = randomOffset;
-    if (random_offset) {
-      offset = Math.floor(Math.random() * 10_000)
-      setRandomOffset(offset);
+    async function update() {
+      setCurQuestion(await get_question(1, questions_offset.current));
     }
-    const qn = curQuestion + 1;
-    const q_id = randomOffset + qn;
-    setWord("omlette_" + qn);
-    setCurQuestion(qn);
+    questions_offset.current = randint(10_000);
+    update();
+  }, [questions_offset]);
 
-    http.get(`/placement-quiz/sample-word/${user}/${q_id}`)
-      .then(res => {
-        setWord(res.word);
-      })
-      .catch(e => {
-        console.error(e);
-      });
-  }
 
-  const respond = (answer) => {
-    const question_answer = {
-      word: word,
+  const respond = async (answer) => {
+    const answer = {
+      word: curQuestion['word'],
       answer: answer
     };
 
-    http.put(`/placement-quiz/answers/${user}`, question_answer)
-      .then(res => { getNextQuestion(); })
-      .catch(e => { 
-        console.log(e);
-        getNextQuestion();
-      });
+    try {
+      await authedHttp().put(`/placement-quiz/answers`, answer)
+    } catch (e) {}
+    var new_q = await get_question(curQuestion['question_num'] + 1, questions_offset.current)
+    setCurQuestion(new_q);
 
     // TODO: Implement an end to the quiz.
   }
@@ -60,10 +60,10 @@ much of your target language you know.`;
     <div style={{margin: '10px'}}>
       <div className="quiz-div">{welcome_blurb}</div>
       <div className="quiz-div">
-        Question {curQuestion}/{num_questions}
+        Question {curQuestion['question_num']}/{num_questions}
       </div>
       <div className="quiz-div">
-      Do you know the word "{word}"?
+      Do you know the word "{curQuestion['word']}"?
       </div>
       <button className="quiz-button" onClick={() => respond("yes")}>Yes</button>
       <button className="quiz-button" onClick={() => respond("sort of")}>Sort of</button>
