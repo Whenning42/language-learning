80afaef4854a65e573bea0f40c600729c848327d
William Henning <wchenningwchenning@gmail.com>
2025-03-19 14:48:49 -0400

    Initial FastAPI route unit test set up

--- analysis_server/main.py
+++ analysis_server/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from enum import Enum
+from typing import Annotated
+
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Language(str, Enum):
+    test_lang = "test_lang"
+    de_DE = "de_DE"
+
+
+class SampleWordRequest(BaseModel):
+    language: Language
+    qid: int
+
+
+class SampleWordResponse(BaseModel):
+    word: str
+
+
+@app.get("/onboarding-quiz/sample-word/", response_model=SampleWordResponse)
+async def sample_word(request: Annotated[SampleWordRequest, Query()]):
+    return {"word": f"{request.language.value}_word_{request.qid}"}
--- analysis_server/main_test.py
+++ analysis_server/main_test.py
@@ -1,16 +1,29 @@
 import unittest
+from enum import Enum
 
 from fastapi.testclient import TestClient
-from main import app
+from main import Language, SampleWordRequest, app
+from pydantic import BaseModel
 
 client = TestClient(app)
 
 
+def pydantic_params(model: BaseModel):
+    model_dict = model.model_dump()
+    model_dict = {
+        k: v.value if isinstance(v, Enum) else v for k, v in model_dict.items()
+    }
+    return model_dict
+
+
 class MyTest(unittest.TestCase):
-    def test_root(self):
-        response = client.get("/")
+    def test_sample_request(self):
+        request = SampleWordRequest(language=Language.test_lang, qid=3)
+        response = client.get(
+            "/onboarding-quiz/sample-word", params=pydantic_params(request)
+        )
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {"message": "Hello World"})
+        self.assertEqual(response.json(), {"word": "test_lang_word_3"})
 
 
 if __name__ == "__main__":
