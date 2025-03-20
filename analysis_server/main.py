1e94305a081291a25b442f159045a6983703408c
William Henning <wchenningwchenning@gmail.com>
2025-03-20 01:55:23 -0400

    Work on implementing placement quiz routes

--- analysis_server/main.py
+++ analysis_server/main.py
@@ -1,15 +1,33 @@
+import datetime
 from enum import Enum
 from typing import Annotated
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Path, Query
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.routing import APIRoute
 from pydantic import BaseModel
 
 app = FastAPI()
+
+# TODO: Lock down CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Language(str, Enum):
     test_lang = "test_lang"
     de_DE = "de_DE"
+
+
+class QuizResponse(str, Enum):
+    correct = "correct"
+    sort_of = "sort of"
+    incorrect = "incorrect"
 
 
 class SampleWordRequest(BaseModel):
@@ -21,6 +39,81 @@ class SampleWordResponse(BaseModel):
     word: str
 
 
-@app.get("/onboarding-quiz/sample-word/", response_model=SampleWordResponse)
+class PlacementQuiz(BaseModel):
+    quiz_id: int
+    user: int
+    language: Language
+    start_time: datetime.datetime
+
+
+class QuizAnswer(BaseModel):
+    # model_config = ConfigDict(use_enum_values=True)
+    answer_num: int
+    word: str
+    answer: QuizResponse
+
+
+class CreatePlacementQuizRequest(BaseModel):
+    user: int
+    language: Language
+
+
+class UpdatePlacementQuizAnswerRequest(BaseModel):
+    answer: QuizAnswer
+    # answer: str
+
+
+class UpdatePlacementQuizAnswerResponse(BaseModel):
+    answer: QuizAnswer
+
+
+@app.get("/placement-quiz/sample-word", response_model=SampleWordResponse)
 async def sample_word(request: Annotated[SampleWordRequest, Query()]):
     return {"word": f"{request.language.value}_word_{request.qid}"}
+
+
+@app.post("/placement-quizzes", response_model=PlacementQuiz)
+async def create_placement_quiz(
+    request: Annotated[CreatePlacementQuizRequest, Query()],
+):
+    # TODO: Mint quiz ID and save this created quiz in the DB
+    placement_quiz = PlacementQuiz(
+        language=request.language,
+        user=request.user,
+        quiz_id=0,
+        start_time=datetime.datetime.now(),
+    )
+    return placement_quiz
+
+
+@app.patch(
+    "/placement-quiz/{quiz_id}/answers/{answer_id}",
+    response_model=UpdatePlacementQuizAnswerResponse,
+)
+async def update_placement_quiz_answer(
+    quiz_id: Annotated[int, Path()],
+    answer_id: Annotated[int, Path()],
+    request: UpdatePlacementQuizAnswerRequest,
+):
+    # TODO: Save response in the DB
+    # return UpdatePlacementQuizAnswerResponse(answer=request.answer)
+    return UpdatePlacementQuizAnswerResponse(
+        answer=QuizAnswer(
+            answer_num=answer_id, word=request.answer.word, answer=request.answer.answer
+        )
+    )
+
+
+def use_route_names_as_operation_ids(app: FastAPI) -> None:
+    """
+    Simplify operation IDs so that generated API clients have simpler function
+    names.
+
+    Should be called only after all routes have been added.
+    """
+    for route in app.routes:
+        if isinstance(route, APIRoute):
+            route.operation_id = route.name
+
+
+use_route_names_as_operation_ids(app)
--- analysis_server/main_test.py
+++ analysis_server/main_test.py
@@ -1,29 +1,43 @@
 import unittest
-from enum import Enum
 
 from fastapi.testclient import TestClient
-from main import Language, SampleWordRequest, app
-from pydantic import BaseModel
+from main import (
+    Language,
+    QuizAnswer,
+    QuizResponse,
+    SampleWordRequest,
+    UpdatePlacementQuizAnswerRequest,
+    UpdatePlacementQuizAnswerResponse,
+    app,
+)
 
 client = TestClient(app)
-
-
-def pydantic_params(model: BaseModel):
-    model_dict = model.model_dump()
-    model_dict = {
-        k: v.value if isinstance(v, Enum) else v for k, v in model_dict.items()
-    }
-    return model_dict
 
 
 class MyTest(unittest.TestCase):
     def test_sample_request(self):
         request = SampleWordRequest(language=Language.test_lang, qid=3)
         response = client.get(
-            "/onboarding-quiz/sample-word", params=pydantic_params(request)
+            "/placement-quiz/sample-word", params=request.model_dump(mode="json")
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"word": "test_lang_word_3"})
+
+    def test_update_placement_quiz_answer_request(self):
+        request = UpdatePlacementQuizAnswerRequest(
+            answer=QuizAnswer(answer_num=5, word="person", answer=QuizResponse.correct)
+        )
+        print("Requesting:", request.model_dump(mode="json"))
+        response = client.patch(
+            "/placement-quiz/10/answers/5", json=request.model_dump(mode="json")
+        )
+        expected = UpdatePlacementQuizAnswerResponse(answer=request.answer)
+        self.assertEqual(
+            UpdatePlacementQuizAnswerResponse.model_validate(
+                response.json()
+            ).model_dump(),
+            expected.model_dump(),
+        )
 
 
 if __name__ == "__main__":
