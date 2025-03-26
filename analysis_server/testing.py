d8e886fb603b9efad590751082b7d5cafcf07675
William Henning <wchenningwchenning@gmail.com>
2025-03-26 01:29:27 -0400

    Improve type correctness of backend testing helpers

--- analysis_server/testing.py
+++ analysis_server/testing.py
@@ -1,5 +1,6 @@
 import unittest
 from datetime import datetime
+from typing import TypeVar
 
 import httpx
 from app import app, get_session
@@ -7,6 +8,8 @@ from fastapi.testclient import TestClient
 from pydantic import BaseModel
 from sqlmodel import Session, SQLModel, create_engine
 from sqlmodel.pool import StaticPool
+
+M = TypeVar("M", bound=BaseModel)
 
 
 class ServerTest(unittest.TestCase):
@@ -28,21 +31,26 @@ class ServerTest(unittest.TestCase):
                 self.client = client
                 super().run(result)
 
-    def assertModelEqual(self, actual: BaseModel, expected: BaseModel):
+    def assertModelEqual(self, actual: M, expected: M) -> None:
         self.assertEqual(actual.__class__, expected.__class__)
         self.assertEqual(actual.model_dump(), expected.model_dump())
 
-    def assertResponseAndGetModel(self, response: httpx.Response, model: BaseModel):
+    def assertResponseAndGetModel(self, response: httpx.Response, model: type[M]) -> M:
         self.assertEqual(response.status_code, 200)
         return model.model_validate(response.json())
 
-    def assertResponseEqual(self, response: httpx.Response, expected: BaseModel):
+    def assertResponseEqual(
+        self, response: httpx.Response, expected: BaseModel
+    ) -> None:
         self.assertEqual(response.status_code, 200)
         expected_model = expected.__class__
         actual = expected_model.model_validate(response.json())
         self.assertModelEqual(actual, expected)
 
-    def assertRecent(self, time: datetime, max_seconds: float = 2):
+    def assertRecent(self, time: datetime | None, max_seconds: float = 2) -> None:
+        self.assertTrue(time is not None)
+        assert time is not None
+
         seconds_ago = (datetime.now() - time).total_seconds()
         self.assertGreaterEqual(seconds_ago, 0)
         self.assertLessEqual(seconds_ago, max_seconds)
