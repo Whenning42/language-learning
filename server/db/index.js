fbb7e69d3eaadce3327bd328c0f37f0149c2cb7b
William Henning <wchenningwchenning@gmail.com>
2025-03-29 15:03:03 -0400

    Delete the express backend

--- server/db/index.js
+++
@@ -1,9 +0,0 @@
-const  Sequelize = require('sequelize');
-const { SqliteDialect } = require('@sequelize/sqlite3');
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'project.db',
-});
-
-module.exports = sequelize;
