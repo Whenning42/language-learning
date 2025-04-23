75a3dcf9106097793b3d19e5db4e1ec149cb3158
William Henning <wchenningwchenning@gmail.com>
2025-04-23 13:08:33 -0400

    Add drizzle studio to the prod DB

--- mobile_app/language-learning/app/db_provider_real.tsx
+++ mobile_app/language-learning/app/db_provider_real.tsx
@@ -2,6 +2,7 @@ import * as SQLite from 'expo-sqlite';
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import { migrate } from 'drizzle-orm/expo-sqlite/migrator';
 import { db_context } from "./db_context";
+import { useDrizzleStudio } from 'expo-drizzle-studio-plugin';
 
 const expoDb = SQLite.openDatabaseSync("db.db");
 const db = drizzle(expoDb);
@@ -12,6 +13,10 @@ export function get_db() {
 }
 
 export default function DBProviderReal({children}) {
+  if (__DEV__) {
+    useDrizzleStudio(expoDb);
+  }
+
   return (<db_context.Provider value={db}>
     {children}
   </db_context.Provider>);
